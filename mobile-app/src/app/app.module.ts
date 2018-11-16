1cf33b7372f466ccf8365046073aa4504bfc6155
Paul Pham <paulpham26@gmail.com>
2018-11-16 14:09:36 -0500

    use component from lib

--- mobile-app/src/app/app.module.ts
+++ mobile-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MobileLibraryModule } from 'mobile-library'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MobileLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
